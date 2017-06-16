5d02cc55f7ae0d0c83d760a8c4c8efccc8c8d637
Adolfo Bello Palacios <adolfob.palacios@icloud.com>
2017-06-15 19:46:32 -0500

    archivos

---
+++ src/demojava/DemoJava.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demojava;
+
+/**
+ *
+ * @author abp392
+ */
+public class DemoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    public static void main(String[] args) {
+        
+        
+        int numero = 0;
+        
+        while(numero <=100)
+        {
+            System.out.println(numero%2 == 0? "el numero" + numero + "es par" : "el numero" + numero + "es impar");
+            numero ++;
+        }
+
+    }
+    
+}
